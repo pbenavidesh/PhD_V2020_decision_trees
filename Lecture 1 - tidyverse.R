a949c6ffadb5deb3440551cc7d10caa84ec2f5bb
pbenavidesh <pbenavides@iteso.mx>
2020-05-26 22:11:07 -0500

    Lecture 1 - tidyverse

--- Lecture 1 - tidyverse.R
+++ Lecture 1 - tidyverse.R
@@ -1,6 +1,19 @@
 library(tidyverse)
 library(gapminder)
 
+seq(-3,3,by=.01) %>%
+  
+  expand.grid(x=., y=.) %>%
+  
+  ggplot(aes(x=(1-x-sin(y^2)), y=(1+y-cos(x^2)))) +
+  
+  geom_point(alpha=.05, shape=20, size=0)+
+  
+  theme_void()+
+  
+  coord_polar()
+
+
 gapminder
 
 gapminder %>% 
@@ -19,3 +32,47 @@ gapminder %>%
   filter(year == 1957) %>% 
   arrange(desc(pop))
 
+gapminder %>% 
+  mutate(lifeExpMonths = lifeExp * 12) %>% 
+  filter(year == 2007) %>% 
+  top_n(10, lifeExpMonths)
+
+plot <- gapminder %>% 
+  filter(year == 2007) %>% 
+  ggplot(aes(x = gdpPercap, y = lifeExp)) +
+  geom_point() +
+  scale_x_log10()
+
+ggplotly(plot)
+
+
+gapminder %>% 
+  ggplot(aes(x = gdpPercap, y = lifeExp,
+             color = continent, size = pop)) +
+  geom_point() +
+  facet_wrap(~ year)
+
+
+gapminder %>% 
+  filter(year == 2007) %>% 
+  summarise(meanLifeExp = mean(lifeExp),
+            totalpop = sum(as.numeric(pop)))
+
+gapminder %>% 
+  summarise(medianLifeExp = median(lifeExp),
+            maxGdpPerCap = max(gdpPercap))
+
+
+gapminder %>% 
+  group_by(year) %>% 
+  summarise(medianLifeExp = median(lifeExp),
+            maxGdp = max(gdpPercap))
+
+gapminder %>% 
+  filter(year == 1957) %>% 
+  group_by(continent) %>% 
+  summarise(medianLifeExp = median(lifeExp),
+            maxGdp = max(gdpPercap))
+
+
+  
