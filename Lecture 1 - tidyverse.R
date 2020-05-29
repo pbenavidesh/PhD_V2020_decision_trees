f96ee6c4c68941ac02c0a7e2f1586406bc4d23b5
pbenavidesh <pbenavides@iteso.mx>
2020-05-29 17:02:34 +0000

    Lecture 1

--- Lecture 1 - tidyverse.R
+++ Lecture 1 - tidyverse.R
@@ -1,6 +1,16 @@
+# Lecture 1 - Intro to tidyverse
+# Analytics based on decision trees for
+# classification and regression
+# Pablo Benavides Herrera
+# 2020-05-26
+
+# pkgs ####
 library(tidyverse)
 library(gapminder)
+library(plotly)
+library(tidymodels)
 
+# A nice figure ####
 seq(-3,3,by=.01) %>%
   
   expand.grid(x=., y=.) %>%
@@ -13,34 +23,49 @@ seq(-3,3,by=.01) %>%
   
   coord_polar()
 
-
+# dplyr + ggplot2 ####
+# the gapminder data
 gapminder
 
+# filtering by year
 gapminder %>% 
   filter(year == 1957)
 
+# filter by country
 gapminder %>% 
   filter(country == "Austria")
 
 gapminder %>% 
   filter(country == "China")
 
+# sorting the tibble by population (descending) 
 gapminder %>% 
   arrange(desc(pop))
 
+# filter + arrange
 gapminder %>% 
   filter(year == 1957) %>% 
   arrange(desc(pop))
 
+# top 10 of countries with max lifeExp (in months)
 gapminder %>% 
   mutate(lifeExpMonths = lifeExp * 12) %>% 
   filter(year == 2007) %>% 
   top_n(10, lifeExpMonths)
 
+# ggplot + plotly
 plot <- gapminder %>% 
   filter(year == 2007) %>% 
   ggplot(aes(x = gdpPercap, y = lifeExp)) +
   geom_point() +
+  scale_x_log10()
+
+ggplotly(plot)
+
+# animate a plot across years
+plot <- gapminder %>% 
+  ggplot(aes(x = gdpPercap, y = lifeExp)) +
+  geom_point(frame = year) +
   scale_x_log10()
 
 ggplotly(plot)
